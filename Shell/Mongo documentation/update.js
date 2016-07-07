4583e1d140dd985d417485815930a2c6143558b6
gthio <gunawanthio@outlook.com>
2016-07-07 23:31:30 +0800

    no message

--- Shell/Mongo documentation/update.js
+++ Shell/Mongo documentation/update.js
@@ -1,1 +1,5 @@
 use testdb;
+
+db.restaurants.find({name: "Morris Park Bake Shop"}).pretty();
+
+db.restaurants.update({name: "Morris Park Bake Shop"}, {$set: {test: "Hi there"}, $currentDate: {lastModified: true}});
