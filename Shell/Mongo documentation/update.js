5e88c07a79ca1a3e6233390d3c026d716e15fc7a
gthio <gunawanthio@outlook.com>
2016-07-08 22:49:44 +0800

    meow

--- Shell/Mongo documentation/update.js
+++ Shell/Mongo documentation/update.js
@@ -3,3 +3,9 @@ use testdb;
 db.restaurants.find({name: "Morris Park Bake Shop"}).pretty();
 
 db.restaurants.update({name: "Morris Park Bake Shop"}, {$set: {test: "Hi there"}, $currentDate: {lastModified: true}});
+
+db.restaurants.find({borough: "Bronx"}).pretty();
+
+//update multiple documents
+db.restaurants.update({borough: "Bronx"}, {$set: {test: "Hi there"}, $currentDate: {lastModified: true}}, {multi: true});
+
