99080cba8fa189f41f1ada7d34eeee9afedae6cd
gunawanthio@dbs.com <gunawanthio@dbs.com>
2016-07-13 14:21:34 +0800

    Clean up c# project

---
+++ DotNet/Src/Course/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using MongoDB.Bson;
+using MongoDB.Driver;
+
+namespace Course
+{
+    class Program
+    {
+        protected static IMongoClient _client;
+        protected static IMongoDatabase _database;
+
+        static void Main(string[] args)
+        {
+            _client = new MongoClient();
+            _database = _client.GetDatabase("testdb");
+        }
+    }
+}
